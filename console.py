21a862ed57d822b8573c0eec848cf4bb295a4013
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-10-12 14:53:47 +0200

    indent fixes

--- console.py
+++ console.py
@@ -8,12 +8,12 @@ class HBNBCommand(cmd.Cmd):
 	prompt = (hbnb)
 	
 	def do_quit(self, arg):
-	"""Exits the command interpreter"""
-		return True
+		"""Exits the command interpreter"""
+			return True
 
 	def do_EOF(self, arg):
-	"""Exits the commnad interpreter with (Ctrl+d)"""
-		return True
+		"""Exits the commnad interpreter with (Ctrl+d)"""
+			return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
