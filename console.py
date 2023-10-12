fe13902f4d0710ab9b4b3b6e59956009d5537f6c
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-10-12 14:40:38 +0200

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defines the HBNB command interperter"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+	""""commnad interprter for HBNB"""
+	
+	prompt = (hbnb)
+	
+	def do_quit(self, arg):
+	"""Exits the command interpreter"""
+		return True
+
+	def do_EOF(self, arg):
+	"""Exits the commnad interpreter with (Ctrl+d)"""
+		return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
