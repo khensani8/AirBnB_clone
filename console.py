db60d99de3cc9c26c2bfe31f0adc80910d2d1e46
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-10-15 12:16:44 +0200

    console 1.0

--- console.py
+++ console.py
@@ -4,10 +4,24 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     @staticmethod
     def convert_type(value):
