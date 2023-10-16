da541848ae91a0b54751b9a295f950da6d882e18
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-10-16 16:57:51 +0200

    console edits

--- console.py
+++ console.py
@@ -1,9 +1,22 @@
+#!/usr/bin/python3
+"""Defines the HBNB console"""
 import cmd
 from models import storage
 from models.base_model import BaseModel
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+
+       Attributes:
+           prompt (str): The command prompt.
+       """
+
     prompt = "(hbnb) "
 
     @staticmethod
